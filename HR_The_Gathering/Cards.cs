8b52d90f42800098cf6f86ca08241b8fe2e4c177
BinaryFly <j3roenvisser@hotmail.com>
2024-03-07 15:43:39 +0100

    Forgot to add Cards.cs to last commit

--- HR_The_Gathering/Cards.cs
+++ HR_The_Gathering/Cards.cs
@@ -1,80 +1,117 @@
 /*
 jeroen visser 0952491
 */
+using System.Collections;
 using Support;
 
-class Cards
+class CardCollection<CardType> : IEnumerable<CardType> where CardType : Card
 {
-    private List<Card> cards;
+    private List<CardType> cards;
 
-    public Cards(List<Card> cards)
+    public CardCollection(IEnumerable<CardType> cards)
     {
-        this.cards = cards;
+        this.cards = cards.ToList();
     }
 
-    // for now no setter
-    // this might be implemented when cards can be added during play to the deck of a player in an expansion
-    public List<Card> GetCards { get { return cards; } }
+    public List<CardType> Cards { get => cards; }
+
+    // these properties allow for nice access to certain types of cards, you could, for example, do the following:
+    // this.Cards.InDeck.Creatures.Red to get all red creatures in the deck, this should work with any other type of combination
+    public CardCollection<CardType> InDeck { get => new CardCollection<CardType>(this.GetCardsThatAre<InDeck>()); } 
+    public CardCollection<CardType> OnBoard { get => new CardCollection<CardType>(this.GetCardsThatAre<OnBoard>()); } 
+    public CardCollection<CardType> InHand { get => new CardCollection<CardType>(this.GetCardsThatAre<InHand>()); } 
+    public CardCollection<CardType> Disposed { get => new CardCollection<CardType>(this.GetCardsThatAre<Disposed>()); } 
+
+    public CardCollection<Creature> Creatures { get => new CardCollection<Creature>(GetCardsOfType<Creature>()); } 
+    public CardCollection<Spell> Spells { get => new CardCollection<Spell>(GetCardsOfType<Spell>()); } 
+    public CardCollection<Land> Lands { get => new CardCollection<Land>(GetCardsOfType<Land>()); } 
+
+    public CardCollection<Card> Red { get => new CardCollection<Card>(GetCardsWithColour<Red>()); } 
+    public CardCollection<Card> Blue { get => new CardCollection<Card>(GetCardsWithColour<Blue>()); } 
+    public CardCollection<Card> Green { get => new CardCollection<Card>(GetCardsWithColour<Green>()); } 
+    public CardCollection<Card> Brown { get => new CardCollection<Card>(GetCardsWithColour<Brown>()); } 
+    public CardCollection<Card> White { get => new CardCollection<Card>(GetCardsWithColour<White>()); } 
+
+    public IEnumerator<CardType> GetEnumerator()
+    {
+        for (int i = 0; i < this.Cards.Count(); i++)
+        {
+            yield return this.Cards[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        for (int i = 0; i < this.Cards.Count(); i++)
+        {
+            yield return this.Cards[i];
+        }
+    }
+    
+    // to get all card types with a certain locationstate
+    private IEnumerable<CardType> GetCardsThatAre<State>() where State : LocationState
+    {
+        return this.Where((card) => card.Location is State).ToList();
+    }
+
+    // to get all cards of a certain type
+    private IEnumerable<T> GetCardsOfType<T>() where T : Card
+    {
+        return (IEnumerable<T>)this.Where((card) => card is T);
+    }
+
+    // to get all cards of a certain colour
+    // we return the IEnumerable with overarching generic type 'Card' here since we can't determine if we only return one cardType because of the way we set colours on lands
+    private IEnumerable<Card> GetCardsWithColour<ColourType>() where ColourType : Colour, new()
+    {
+        var colourToGet = ColourCreator.GetColour<ColourType>();
+        // we have to get the colour of lands separately since they have NoCost and this doesn't have a specific colour
+        var landsWithColour = this.Lands.Where((card) => card.Colour == colourToGet);
+        var otherCardsWithColour = this.Where((card) => card.Cost.Contains(colourToGet));
+
+        var allCardsWithColour = landsWithColour.Concat<Card>(otherCardsWithColour);
+
+        return allCardsWithColour;
+
+    }
+}
+
+
+// this is the class for the base deck of the player
+class Cards : CardCollection<Card>
+{
+    public Cards(IEnumerable<Card> cards) : base(cards) { }
 
     /// <summary>
     /// Checks if the cards are valid by checking if any cards have more than 3 duplicates in the collection
     /// </summary>
     public bool HasTooManyDuplicates()
     {
-        return this.cards
+        return this.Cards
             .GroupBy(card => card.Id)
             .Any(cardSet => cardSet.Count() > 3);
     }
 
     public bool DeckIsEmpty()
     {
-        return GetCardsThatAre<InDeck>().Count() == 0;
-    }
-
-    /// <summary>
-    /// With this we can get all the cards that have a certain <c>LocationState</c> in the collection
-    /// </summary>
-    public List<Card> GetCardsThatAre<State>() where State : LocationState
-    {
-        return this.cards.Where((card) => card.Location is State).ToList();
-    }
-
-    /// <summary>
-    /// This can be handy to get all the cards that are of a certain cardtype, like getting all lands on board  
-    /// </summary>
-    public List<CardType> GetCardsThatAre<CardType, State>() where State : LocationState where CardType : Card
-    {
-        return this.cards.Where((card) => card is CardType && card.Location is State).Select((card) => (CardType)card).ToList();
-    }
-
-    public List<CardType> GetCardsThatAre<CardType, ColourType, State>() where CardType : Card where State : LocationState where ColourType : Colour, new()
-    {
-        var colourToCheck = ColourCreator.GetColour<ColourType>();
-        return this.cards.Where((card) =>
-        {
-            var cardIsType = card is CardType;
-            var cardHasState = card.Location is State;
-            var costContainsColour = card.Cost.Contains(colourToCheck);
-            var isLandAndContainsColour = card is Land && ((Land)card).Colour == colourToCheck;
-            return cardIsType && cardHasState && (costContainsColour || isLandAndContainsColour);
-        }).Select((card) => (CardType)card).ToList();
+        return this.InDeck.Count() == 0;
     }
 
     public override string ToString()
     {
-        var cardsOnBoard = GetCardsThatAre<OnBoard>().Select((card) => card.Id);
+        var cardsOnBoard = this.OnBoard.Select((card) => card.Id);
         var onBoardCount = cardsOnBoard.Count();
         var onBoardString = onBoardCount != 0 ? cardsOnBoard.Aggregate((acc, next) => acc + ", " + next) : "";
 
-        var cardsInHand = GetCardsThatAre<InHand>().Select((card) => card.Id);
+        var cardsInHand = this.InHand.Select((card) => card.Id);
         var inHandCount = cardsInHand.Count();
         var inHandString = inHandCount != 0 ? cardsInHand.Aggregate((acc, next) => acc + ", " + next) : "";
 
-        var cardsInDeck = GetCardsThatAre<InDeck>().Select((card) => card.Id);
+        var cardsInDeck = this.InDeck.Select((card) => card.Id);
         var inDeckCount = cardsInDeck.Count();
         var inDeckString = inDeckCount != 0 ? cardsInDeck.Aggregate((acc, next) => acc + ", " + next) : "";
 
-        var cardsDisposed = GetCardsThatAre<Disposed>().Select((card) => card.Id);
+        var cardsDisposed = this.Disposed.Select((card) => card.Id);
         var disposedCount = cardsDisposed.Count();
         var disposedString = disposedCount != 0 ? cardsDisposed.Aggregate((acc, next) => acc + ", " + next) : "";
 
